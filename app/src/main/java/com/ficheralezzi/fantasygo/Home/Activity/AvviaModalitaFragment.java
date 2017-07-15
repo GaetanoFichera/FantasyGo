b73215945c010cacbed910ec4830fb08e6ac4dfc
Gaetano Fichera <gaetano.fichera93@gmail.com>
2017-07-15 15:11:54 +0200

    aggiunto AvviaModalitFragment.java al git

---
+++ app/src/main/java/com/ficheralezzi/fantasygo/Home/Activity/AvviaModalitaFragment.java
@@ -0,0 +1,35 @@
+package com.ficheralezzi.fantasygo.Home.Activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.ficheralezzi.fantasygo.R;
+
+/**
+ * Created by gaetano on 14/07/17.
+ */
+
+public class AvviaModalitaFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_avvia_modalita, container, false);
+        setButtonListener(view);
+        return view;
+    }
+
+    private void setButtonListener(View v){
+        Button button = ((Button) v.findViewById(R.id.button_avvia_modNearPvE));
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((com.ficheralezzi.fantasygo.Home.Activity.SwipeHomeActivity) getActivity()).goToModNearPve();
+            }
+        });
+    }
+}
